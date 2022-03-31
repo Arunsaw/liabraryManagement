4d10ee44a2a3530c0fb23442c58ebe18e593f5e7
Arunsaw <31052412+Arunsaw@users.noreply.github.com>
2022-03-31 19:03:52 +0530

    Add files via upload

--- LiabraryManagementApplication/src/main/java/com/liabrarymanagement/srcode/servicesInterface/BookService.java
+++ LiabraryManagementApplication/src/main/java/com/liabrarymanagement/srcode/servicesInterface/BookService.java
@@ -1,5 +1,17 @@
 package com.liabrarymanagement.srcode.servicesInterface;
 
-public interface BookService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.liabrarymanagement.srcode.entity.Books;
+@Service
+public interface BookService {
+	
+	public  void saveBook(Books b) ;
+	public List<Books> listAll();
+	public void deleteBook(Long id);
+	public void update(Long id , Books b);
+	public Books get(Long id);
+	
 }
